1e93b41dd400c8c034d5e5701f6bdc43135f35bc
Thanatad <meoler31@gmail.com>
2022-08-04 12:46:52 +0700

    refactor config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,39 +1,14 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import 'dotenv/config';
 import { DataSourceOptions } from 'typeorm';
 import {
     TypeOrmModuleAsyncOptions,
     TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 
-export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: async (): Promise<TypeOrmModuleOptions> => {
-        return {
-            type: 'mysql',
-            host: process.env.DB_HOST,
-            port: parseInt(process.env.DB_PORT, 10),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            synchronize: false,
-            entities: [`${__dirname}/../**/*.entity.{js,ts}`],
-            migrationsRun: false,
-            logging: true,
-            migrationsTableName: "migration",
-            migrations: [`${__dirname}/../database/migrations/*.{js,ts}`],
-            extra: {
-                charset: 'utf8mb4_unicode_ci',
-            }
-        };
-    },
 
-};
-
-
-export const typeOrmConfig: DataSourceOptions = {
+export const databaseConfig: DataSourceOptions = {
     type: 'mysql',
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10),
@@ -48,6 +23,16 @@ export const typeOrmConfig: DataSourceOptions = {
     migrations: [`${__dirname}/../database/migrations/*.{js,ts}`],
     extra: {
         charset: 'utf8mb4_unicode_ci',
-    },
+    }
 }
 
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (): Promise<TypeOrmModuleOptions> => {
+        return databaseConfig
+    },
+
+};
+
+export const typeOrmConfig: DataSourceOptions = databaseConfig
