17cdef973bc4697ce9d5dbfd4e9a2cbba20d6b07
Thanatad <meoler31@gmail.com>
2022-08-08 08:38:25 +0700

    fixed redirect controller

--- src/urlshort/controller/redirect.controller.ts
+++ src/urlshort/controller/redirect.controller.ts
@@ -10,7 +10,7 @@ export class RedirectController {
     async redirect(@Res() response, @Param() paramQuery: GetUrlshortQuery) {
         try {
             const urlshort = await this.urlshortService.findByFilter({ code: paramQuery.code });
-            return response.redirect(urlshort[0].url);
+            return response.redirect(urlshort.data[0].url);
         } catch (error) {
             return response.redirect('https://xhofficial.com/');
         }
